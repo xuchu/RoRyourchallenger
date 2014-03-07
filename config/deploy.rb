dadd0c36de95e5a0fc77d31d4855c2e3570fe6a2
Fred <frouds@gmail.com>
2014-03-07 14:20:28 -0600

    test cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,7 +52,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
+      execute "touch #{ File.join(current_path, 'tmp', 'restart.txt'}"
     end
   end
 
